355aea185834e9ea7097736b2aecef4d182786c3
FelipeRibeir0 <fecribeiro2003@gmail.com>
2023-09-29 19:09:28 -0300

    add alert || add edit function

--- js/app.js
+++ js/app.js
@@ -1,18 +1,25 @@
 const listContainer = document.getElementById("list-container");
 const inputBox = document.getElementById("input-box");
 const Submitbutton = document.getElementById("submit-button");
+const messageBox = document.getElementById("hidden");
 
 function addTask() {
   if (inputBox.value == "") {
-    alert("É necessário que você escreva algo.");
+    messageBox.id = "alert";
   } else {
-    let li = document.createElement("li");
+    messageBox.id = "hidden";
+    const li = document.createElement("li");
     li.innerHTML = inputBox.value;
     listContainer.appendChild(li);
+
+    const deleteIcon = document.createElement("i");
+    deleteIcon.className = "fa-solid fa-trash-can";
+    li.appendChild(deleteIcon);
+
+    const editIcon = document.createElement("i");
+    editIcon.className = "fa-solid fa-pen";
+    li.appendChild(editIcon);
     inputBox.value = "";
-    let span = document.createElement("span");
-    span.innerHTML = "\u00d7";
-    li.appendChild(span);
   }
   saveTasks();
 }
@@ -21,20 +28,55 @@ function saveTasks() {
   localStorage.setItem("tasks", listContainer.innerHTML);
 }
 
+function editTask(li) {
+  const inputEdit = document.createElement("input");
+  const trashIcon = document.createElement("i");
+  trashIcon.className = "fa-solid fa-trash-can";
+
+  const editIcon = document.createElement("i");
+  editIcon.className = "fa-solid fa-pen";
+
+  inputEdit.value = li.innerText;
+  inputEdit.maxLength = 35;
+  li.innerHTML = "";
+  li.appendChild(inputEdit);
+  li.appendChild(trashIcon);
+  li.appendChild(editIcon);
+
+  inputEdit.addEventListener("keyup", function (e) {
+    if (e.key === "Enter") {
+      li.innerHTML = inputEdit.value;
+      li.appendChild(trashIcon);
+      li.appendChild(editIcon);
+      saveTasks();
+    }
+  });
+
+  inputEdit.addEventListener("blur", function () {
+    li.innerHTML = inputEdit.value;
+    li.appendChild(trashIcon);
+    li.appendChild(editIcon);
+    saveTasks();
+  });
+
+  inputEdit.focus();
+}
+
 listContainer.addEventListener("click", function (e) {
   if (e.target.tagName === "LI") {
     e.target.classList.toggle("checked");
-  } else if (e.target.tagName === "SPAN") {
-    console.log(e.target.parentElement);
+  } else if (e.target.className === "fa-solid fa-trash-can") {
     e.target.parentElement.remove();
+  } else if (e.target.className === "fa-solid fa-pen") {
+    editTask(e.target.parentElement);
   }
   saveTasks();
 });
 
 Submitbutton.addEventListener("click", addTask);
 
-inputBox.addEventListener("keyup", function (event) {
-  if (event.key === "Enter") {
+inputBox.addEventListener("keyup", function (e) {
+  if (e.key === "Enter") {
     addTask();
   }
 });
